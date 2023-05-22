f85ab36242688517904f34a9a1ec70b462835972
CHOWKUIHENG <kuiheng1018@gmail.com>
2023-05-22 15:52:33 +0800

    finish exercise

---
+++ randomnumber.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="randomnumber.css" />
+    <title>Random Number</title>
+</head>
+
+<?php
+$number1 = rand(100, 200);
+$number2 = rand(100, 200);
+$sum = $number1 + $number2;
+$multiple = $number1 * $number2;
+?>
+
+<body>
+    <p class="italic green"><?php echo $number1; ?></p>
+    <p class="italic blue"><?php echo $number2; ?></p>
+    <p class="bold red"><?php echo $sum; ?></p>
+    <p class="bold italic"><?php echo $multiple; ?></p>
+</body>
+
+</html>
