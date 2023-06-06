67df9847e52dc8412f1325fc1cef9d5ad19d8afa
CHOWKUIHENG <kuiheng1018@gmail.com>
2023-06-06 21:34:17 +0800

    open a week4 folder and doing question1

---
+++ week4/name.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 4 Question 1</title>
+</head>
+
+<body>
+    <h2>Question 1</h2>
+    <form method="POST">
+        <label for="firstName">First Name:</label>
+        <input type="text" id="firstName" name="firstName" required><br><br>
+
+        <label for="lastName">Last Name:</label>
+        <input type="text" id="lastName" name="lastName" required><br><br>
+
+        <input type="submit" name="submit" value="Submit">
+    </form>
+
+    <?php
+    if (isset($_POST['submit'])) {
+        $firstName = $_POST['firstName'];
+        $lastName = $_POST['lastName'];
+        $formattedFirstName = ucwords(strtolower($firstName));
+        $formattedLastName = ucwords(strtolower($lastName));
+
+        echo "Name : $formattedFirstName $formattedLastName";
+    }
+    ?>
+</body>
+
+</html>
