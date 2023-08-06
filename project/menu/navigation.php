340d7df50124034bf748d098f4cdd86def86df8f
CHOWKUIHENG <kuiheng1018@gmail.com>
2023-08-06 12:22:28 +0800

    structure code on line 44

--- project/menu/navigation.php
+++ project/menu/navigation.php
@@ -41,7 +41,7 @@
                 </ul>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="contact_form.php">Contact us</a>
+                <a class="nav-link" href="contactus.php">Contact us</a>
             </li>
         </ul>
     </div>
