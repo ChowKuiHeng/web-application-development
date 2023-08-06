4727802d4ea64b6f6f61e0e9b7339f33070e3640
CHOWKUIHENG <kuiheng1018@gmail.com>
2023-08-06 12:09:30 +0800

    structure code in line 66,99,104,106

--- project/customer_update.php
+++ project/customer_update.php
@@ -3,43 +3,32 @@
 
 <head>
     <title>Customer Update</title>
+    <!-- Latest compiled and minified Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 
 <body>
-    <!-- Navigation Menu -->
-    <?php
-    include 'menu/navigation.php';
-    ?>
     <!-- container -->
     <div class="container">
+        <?php include 'menu/navigation.php'; ?>
         <div class="page-header">
             <h1>Update Customer</h1>
         </div>
         <?php
-        // get passed parameter value, in this case, the record ID
-        // isset() is a PHP function used to verify if a value is there or not
         $id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
 
-        //include database connection
         include 'config/database.php';
 
-        // read current record's data
         try {
-            // prepare select query
             $query = "SELECT id, username, password, firstname, lastname, email, gender, date_of_birth, account_status FROM customers WHERE id = ? LIMIT 0,1";
             $stmt = $con->prepare($query);
 
-            // this is the first question mark
             $stmt->bindParam(1, $id);
 
-            // execute our query
             $stmt->execute();
 
-            // store retrieved row to a variable
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            // values to fill up our form
             $username = $row['username'];
             $password = $row['password'];
             $firstname = $row['firstname'];
@@ -50,48 +39,47 @@
             $account_status = $row['account_status'];
         }
 
-        // show error
+        // show errors
         catch (PDOException $exception) {
             die('ERROR: ' . $exception->getMessage());
         }
-        ?>
-
-        <?php
-        // check if form was submitted
         if ($_POST) {
             try {
                 $query = "UPDATE customers
-                SET username=:username, password=:password, firstname=:firstname, lastname=:lastname, gender=:gender, date_of_birth=:date_of_birth, account_status=:account_status, email=:email WHERE id = :id";
-                // prepare query for execution
-                $stmt = $con->prepare($query);
+                SET username=:username, firstname=:firstname, lastname=:lastname, gender=:gender, date_of_birth=:date_of_birth, email=:email, account_status=:account_status";
+                // prepare query for excecution
+
                 // posted values
                 $username = htmlspecialchars(strip_tags($_POST['username']));
-                $old_password = htmlspecialchars(strip_tags($_POST['old_password']));
-                $new_password = htmlspecialchars(strip_tags($_POST['new_password']));
-                $confirm_password = htmlspecialchars(strip_tags($_POST['confirm_password']));
+                $old_password = $_POST['old_password'];
+                $new_password = $_POST['new_password'];
+                $confirm_password = $_POST['confirm_password'];
                 $firstname = htmlspecialchars(strip_tags($_POST['firstname']));
                 $lastname = htmlspecialchars(strip_tags($_POST['lastname']));
+                $gender = $_POST['gender'];
+                $date_of_birth = $_POST['date_of_birth'];
                 $email = htmlspecialchars(strip_tags($_POST['email']));
-                $gender = htmlspecialchars(strip_tags($_POST['gender']));
-                $date_of_birth = htmlspecialchars(strip_tags($_POST['date_of_birth']));
-                $account_status = htmlspecialchars(strip_tags($_POST['account_status']));
+                $account_status = $_POST['account_status'];
 
                 $errors = array();
+                // check !empty, check format, compare new and confirm, then old compare db, old compare new.(password)
+                if (!empty($_POST['old_password']) && !empty($_POST['new_password']) && !empty($_POST['confirm_password'])) {
+                    if ($new_password == $confirm_password) {
+                        if (password_verify($old_password, $password)) {
 
-                if (!empty($_POST['old_password'])) {
-                    if (password_verify($old_password, $row['password'])) {
-                        if ($new_password == $old_password) {
-                            $errors[] = "New Password cannot same with old password.";
-                        } else if ($new_password == $confirm_password) {
-                            $formatted_password = password_hash($new_password, PASSWORD_DEFAULT);
+                            if ($old_password == $new_password) {
+                                $errors[] = "New password can't be same with old password";
+                            } else {
+                                $hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
+                            }
                         } else {
-                            $errors[] = "Confirm password does not match with new password.";
+                            $errors[] = "Wrong password entered in old password column";
                         }
                     } else {
-                        $errors[] = "You entered the wrong password for old password";
+                        $errors[] = "The confirm password doesn't match with new password.";
                     }
                 } else {
-                    $formatted_password = $password;
+                    $hashed_password = $password;
                 }
 
                 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -100,21 +88,28 @@
 
                 if (!empty($errors)) {
                     echo "<div class='alert alert-danger'>";
-                    foreach ($errors as $error) {
-                        echo $error . "<br>";
+                    foreach ($errors as $errorMessage) {
+                        echo $errorMessage . "<br>";
                     }
                     echo "</div>";
                 } else {
-                    // bind the parameters
-                    $stmt->bindParam(':id', $id);
+                    if (isset($hashed_password)) {
+                        $query .= ", password=:password";
+                    }
+
+                    $query .= " WHERE id=:id";
+                    $stmt = $con->prepare($query);
+                    $stmt->bindParam(":id", $id);
                     $stmt->bindParam(':username', $username);
-                    $stmt->bindParam(':password', $formatted_password);
+                    if (isset($hashed_password)) {
+                        $stmt->bindParam(':password', $hashed_password);
+                    }
                     $stmt->bindParam(':firstname', $firstname);
                     $stmt->bindParam(':lastname', $lastname);
                     $stmt->bindParam(':gender', $gender);
                     $stmt->bindParam(':date_of_birth', $date_of_birth);
-                    $stmt->bindParam(':account_status', $account_status);
                     $stmt->bindParam(':email', $email);
+                    $stmt->bindParam(':account_status', $account_status);
                     // Execute the query
                     if ($stmt->execute()) {
                         echo "<div class='alert alert-success'>Record was updated.</div>";
@@ -125,17 +120,15 @@
             }
             // show errors
             catch (PDOException $exception) {
-                die('ERROR: ' . $exception->getMessage());
+                echo "<div class='alert alert-danger'>ERROR: " . $exception->getMessage() . "</div>";
             }
         } ?>
 
-
-        <!-- HTML form to update record will be here -->
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?id={$id}"); ?>" method="post">
             <table class='table table-hover table-responsive table-bordered'>
                 <tr>
-                    <td>Username</td>
-                    <td><input type='text' name='username' value="<?php echo htmlspecialchars($username, ENT_QUOTES); ?>" class='form-control' /></td>
+                    <td>User Name</td>
+                    <td><input type='text' name='username' value="<?php echo htmlspecialchars($username, ENT_QUOTES);  ?>" class='form-control' /></td>
                 </tr>
                 <tr>
                     <td>Old Password</td>
