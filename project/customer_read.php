d6b78a0b1e72ee8f63134721e4d8b0dde7bed9ac
CHOWKUIHENG <kuiheng1018@gmail.com>
2023-08-06 22:47:05 +0800

    structure code on line 108

--- project/customer_read.php
+++ project/customer_read.php
@@ -105,7 +105,7 @@
                 echo "<a href='customer_read_one.php?id={$id}' class='btn btn-info me-3'>Read</a>";
 
                 // we will use these links in the next part of this post
-                echo "<a href='product_update.php?id={$id}' class='btn btn-primary me-3'>Edit</a>";
+                echo "<a href='customer_update.php?id={$id}' class='btn btn-primary me-3'>Edit</a>";
 
                 // we will use this links in the next part of this post
                 echo "<a href='#' onclick='delete_product({$id});' class='btn btn-danger me-3'>Delete</a>";
